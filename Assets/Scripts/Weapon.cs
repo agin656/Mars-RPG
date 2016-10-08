99cf35d81bb747d1f438bfbe9785e06551e1e50e
Andrew Gin <agin656@gmail.com>
2016-10-08 11:05:08 -0400

    added ranged weapon functionality

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,9 +8,9 @@ public class Weapon {
 
     public string weaponName = "fists";
     public float damage = 10;
-    public bool melee = true;
-    public float range = 5;
-    public float cooldown = 0.5f;
+    public bool melee = false;
+    public float range = 25;
+    public float cooldown = 0.1f;
 
     private bool ready = true;
     private bool active = true;
@@ -18,6 +18,11 @@ public class Weapon {
 
     public void Attack()
     {
+        if(!melee)
+        {
+            Shoot();
+            return;
+        }
         if (!active) return;
         if (!ready)
         {
@@ -52,6 +57,74 @@ public class Weapon {
         }
     }
 
+    public void Shoot()
+    {
+        if (!active) return;
+        if (!ready)
+        {
+            if (Time.time - currentTime > cooldown)
+            {
+                ready = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        //I'm keeping all of the commented out stuff for now because while it was buggy, it did solve some problems that I was having before with raycasting
+        Vector3 fireDirection = owner.gameObject.transform.forward;
+        //RaycastHit[] allHits;
+        RaycastHit hit;
+        Vector3 loc = owner.gameObject.transform.position;
+        //allHits = Physics.SphereCastAll(loc, 0.5f, fireDirection, range);
+        //allHits = Physics.RaycastAll(loc, fireDirection, range);
+        //allHits = sortHits(allHits);
+        /*foreach (var hit in allHits)
+        {
+            if (hit.transform.position != owner.gameObject.transform.position)
+            {
+                Debug.Log(hit.collider.tag);
+                try
+                {
+                    hit.rigidbody.gameObject.SendMessage("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+                }
+                catch
+                {
+                    return;
+                }
+                hit.rigidbody.gameObject.SendMessage("ApplyKnockback", owner.gameObject.transform.position, SendMessageOptions.DontRequireReceiver);
+                currentTime = Time.time;
+                ready = false;
+            }
+            if (hit.transform.root != owner.transform.root)
+            {
+                return;
+            }
+        }*/
+        if (Physics.SphereCast(loc, 0.5f, fireDirection, out hit, range))
+        {
+            if (hit.transform.position != owner.gameObject.transform.position)
+            {
+                try
+                {
+                    hit.rigidbody.gameObject.SendMessage("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+                }
+                catch
+                {
+                    return;
+                }
+                hit.rigidbody.gameObject.SendMessage("ApplyKnockback", owner.gameObject.transform.position, SendMessageOptions.DontRequireReceiver);
+                currentTime = Time.time;
+                ready = false;
+            }
+            if (hit.transform.root != owner.transform.root)
+            {
+                return;
+            }
+        }
+    }
+
     public void Deactivate()
     {
         if (model != null) model.SetActive(false);
@@ -63,4 +136,27 @@ public class Weapon {
         if (model != null) model.SetActive(true);
         active = true;
     }
+
+    /*private RaycastHit[] sortHits(RaycastHit[] hits)
+    {
+        int minIndex = 0;
+        for (int i = 0; i < hits.Length; i++)
+        {
+            float min = range;
+            for (int j = 0; j < hits.Length; j++)
+            {
+                var hit = hits[j];
+                if (hit.distance < min)
+                {
+                    min = hit.distance;
+                    minIndex = j;
+                }
+            }
+            var current = hits[i];
+            var minHit = hits[minIndex];
+            hits[i] = minHit;
+            hits[minIndex] = minHit;
+        }
+        return hits;
+    }*/
 }
